e683046dbad108196010250669f9cce820862bf9
Evan N <dacsang97@gmail.com>
2018-12-10 21:21:41 +0700

    fix xo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as express from 'express';
+import * as express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (_, res) => {
-	res.send('Hello');
-});
+  res.send('Hello')
+})
 
 app.listen(3000, () => {
-	console.log('App running on port 3000');
-});
+  console.log('App running on port 3000')
+})
